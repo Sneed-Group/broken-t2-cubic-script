d7bf460ded12414530a9ba1a18d5d86ba2068e1b
nodemixaholic <sam@sllord.info>
2024-02-20 20:42:44 +0000

    Add 't2-debian.sh'

---
+++ t2-debian.sh
@@ -0,0 +1,13 @@
+curl -s --compressed "https://adityagarg8.github.io/t2-ubuntu-repo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/t2-ubuntu-repo.gpg >/dev/null
+sudo curl -s --compressed -o /etc/apt/sources.list.d/t2.list "https://adityagarg8.github.io/t2-ubuntu-repo/t2.list"
+sudo apt update
+sudo apt install t2-kernel-script-debian
+sudo update_t2_kernel
+sudo apt install apple-t2-audio-config tiny-dfr zstd
+wget http://nodemixaholic.com:3002/nodemixaholic/apple-broadcom-firmware-arch/raw/branch/main/apple-bcm-firmware-14.0-1-any.pkg.tar.zst
+zstd -d -c apple-bcm-firmware-14.0-1-any.pkg.tar.zst | tar -xvf -
+sudo cp -r usr/* /usr/
+sudo apt install iwd
+sudo sed -i 's/^wifi\.backend.*/wifi.backend=iwd/' /etc/NetworkManager/NetworkManager.conf
+sudo systemctl enable --now iwd
+sudo systemctl restart NetworkManager
