e96328767a1933b9a6422c9b4fbf0ff77958dc2f
nodemixaholic <sam@sllord.info>
2024-02-21 00:14:39 +0000

    Update 't2-debian.sh'

--- t2-debian.sh
+++ t2-debian.sh
@@ -1,4 +1,5 @@
 sudo apt install curl git
+sudo apt install task-cinnamon-desktop sddm
 curl -s --compressed "https://adityagarg8.github.io/t2-ubuntu-repo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/t2-ubuntu-repo.gpg >/dev/null
 sudo curl -s --compressed -o /etc/apt/sources.list.d/t2.list "https://adityagarg8.github.io/t2-ubuntu-repo/t2.list"
 sudo apt update
